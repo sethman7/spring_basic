463bd0066c192531bd280c912330f861d2df9849
sethman7 <131145377+sethman7@users.noreply.github.com>
2026-06-12 18:16:03 +0900

    DB connect

--- src/main/java/studio/thinkground/springbasic/SpringConfig.java
+++ src/main/java/studio/thinkground/springbasic/SpringConfig.java
@@ -2,12 +2,22 @@ package studio.thinkground.springbasic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import studio.thinkground.springbasic.repository.JdbcMemberRepository;
 import studio.thinkground.springbasic.repository.MemberRepository;
 import studio.thinkground.springbasic.repository.MemoryMemberRepository;
 import studio.thinkground.springbasic.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import javax.sql.DataSource;
+
 
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,7 +26,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
 }
